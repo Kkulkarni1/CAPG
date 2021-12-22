9bb856b1f5187a8f98c54bed30ed6a09063f5cbe
YudiZhang <yudizhang628@gmail.com>
2021-12-21 20:29:39 -0600

    - add example

--- script_main/ksd-ksd/matrix_exponential.h
+++
@@ -1,6 +0,0 @@
-/*
-  Real functions.
-*/
-double *r8mat_expm1 ( int n, double a[] );
-double *r8mat_expm2 ( int n, double a[] );
-double *r8mat_expm3 ( int n, double a[] );
--- script_main/ksd-ksd/statistics.c
+++
@@ -1,9 +0,0 @@
-#include "statistics.h"
-
-double aic(double ll, size_t k) {
-	return (2*k - 2*ll);
-} /* aic */
-
-double bic(double ll, size_t k, size_t n) {
-	return (log(n)*k - 2*ll);
-} /* bic */
